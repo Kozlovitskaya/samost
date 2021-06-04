4d904a350974b25bd7244fcefe1035f43b34f94d
wsruser1 <ninavolga96@gmail.com>
2021-06-04 15:21:07 +0300

    Initial Commit

---
+++ samost/samostApp.swift
@@ -0,0 +1,17 @@
+//
+//  samostApp.swift
+//  samost
+//
+//  Created by user on 04.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct samostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
