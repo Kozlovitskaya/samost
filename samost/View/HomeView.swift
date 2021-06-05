e9c4f4ed7390fe0aacbb436a8f7327072301888d
wsruser1 <ninavolga96@gmail.com>
2021-06-05 16:25:58 +0300

    5

---
+++ samost/View/HomeView.swift
@@ -0,0 +1,40 @@
+//
+//  HomeView.swift
+//  samost
+//
+//  Created by user on 05.06.2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack{
+            HStack {
+                Image(systemName: "bookmark")
+                Spacer()
+                Text("Social").font(.title)
+                Spacer()
+                Image(systemName: "bell")
+            }.padding()
+            ScrollView(.horizontal, showsIndicators: false,
+                       content: {
+                        HStack {
+                            ForEach(0...15, id: \.self) { _ in
+                                Image("planeta")
+                                    .resizable()
+                                    .frame(width: 75, height: 75, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            }
+                            
+                        }.border(Color.black)
+            })
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
